166497507d07609ff1b7d6260dd89695ad1f070f
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-02-19 23:27:57 +0545

    Routers

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter,Route,Routes,Link } from 'react-router-dom';
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+
+function App(){
+    return(
+        <div>
+            <div className=''>
+                <h1 className='text-2xl flex justify-center'>Fill out the form to enter.</h1>
+                <BrowserRouter>
+                <div className='p-28'>
+
+                <p className=''><Link to='/' className="bg-green-600 p-4 rounded-lg">Login</Link> | <Link to="profile" className="bg-green-600 p-4 rounded-lg">Profile</Link></p>
+                </div>
+                <Routes>
+                    <Route path='/' element={<Login/>}/>
+                    <Route path='/profile' element={<Profile/>}/>
+                </Routes>
+                </BrowserRouter>
+            </div>
+        </div>
+    )
+}
+
+export default App;
