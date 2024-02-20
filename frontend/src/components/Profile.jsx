1b7f43f6673e926e0b9ac0eb08d3c9ef0d320141
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-02-20 20:35:09 +0545

    AddedSignOut

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Profile() {
+  const navigate=useNavigate();
+  const signOut=()=>{
+    localStorage.removeItem('token');
+    navigate('/');
+  }
   return (
     <div>
 
     <div>Profile</div>
-    
+    <p>this is your profile </p>
+    <div>
+      <button type='button' onClick={signOut}>Sign Out</button>
+    </div>
     </div>
   )
 }
